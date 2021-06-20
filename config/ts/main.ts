119c31ed74de3529fffc247a5c1370fada00fc26
hiroyuki <mk.9.q.kyu@gmail.com>
2021-06-20 14:18:58 +0900

    update template

--- config/ts/main.ts
+++ config/ts/main.ts
@@ -1,23 +1,25 @@
 "use strict";
 {
-  const main = (input) => {
-    const r = _io(input);
+  const main = (): number | string | void => {
+    const r = _io();
 
     return 0;
   };
 
-  const _io = (str) => {
+  const _io = () => {
+    const str: string = require("fs").readFileSync("/dev/stdin", "utf8");
     const lines = str.trim().split("\n");
-    const s = () => lines.shift() || "";
-    const n = () => Number(s());
-    const ss = () => s().split(" ");
-    const ns = () => ss().map(Number);
-    const nls = () => lines.map(Number);
-    return { lines, s, n, ss, ns, nls };
+    const s = (): string => lines.shift() || "";
+    const n = (): number => Number(s());
+    const ss = (): string[] => s().split(" ");
+    const ns = (): number[] => ss().map(Number);
+    const nls = (): number[] => lines.map(Number);
+    const nla = (): number[][] => lines.map(line => line.split(" ").map(Number));
+
+    return { lines, s, n, ss, ns, nls, nla };
   };
 
-  const _getInput = () => require("fs").readFileSync("/dev/stdin", "utf8");
-  const _res = main(_getInput());
+  const _res = main();
 
   if (typeof _res === "number" || typeof _res === "string") console.log(_res);
 }

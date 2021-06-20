3bcc92de11610f1a4aeef4978db397e84aa44719
hiroyuki <mk.9.q.kyu@gmail.com>
2021-06-20 14:04:45 +0900

    update

--- config/deploy.sh
+++ config/deploy.sh
@@ -2,16 +2,18 @@
 ACCDIR=`acc config-dir`
 DIR=`pwd`
 
-for f in *
-do
+for f in *; do
   [ "$f" = "deploy.sh" ] && continue
   if [ -f $f ] ; then
-    cp -f "$DIR/$f" "$ACCDIR"/"$f"
+    ln -snfv "$DIR/$f" "$ACCDIR"/"$f"
   elif [ -d $f ] ; then
-    for ff in `ls "$f/"`
-    do
-      mkdir -p "$ACCDIR/$f"
-      cp -f "$DIR/$f/$ff" "$ACCDIR/$f/$ff"
+    for ff in `ls "$f/"`; do
+        mkdir -p "$ACCDIR/$f"
+      if [ "$ff" = "template.json" ] ; then
+        ln -snfv "$DIR/$f/$ff" "$ACCDIR/$f/$ff"
+      else
+        cp -f "$DIR/$f/$ff" "$ACCDIR/$f/$ff"
+      fi
     done
   fi
 done

0a7b14b417031acca6eb83a058af5bd45c764c02
hiroyuki <mk.9.q.kyu@gmail.com>
2021-06-26 22:45:07 +0900

    abc207のABC
    B難しかった...

---
+++ src/abc207/a/main.ts
@@ -0,0 +1,27 @@
+"use strict";
+{
+  const main = (): number | string | void => {
+    const r = _io();
+    const nums = r.ns().sort((a, b) => b - a);
+
+    return nums[0] + nums[1];
+  };
+
+  const _io = () => {
+    const str: string = require("fs").readFileSync("/dev/stdin", "utf8");
+    const lines = str.trim().split("\n");
+    const s = (): string => lines.shift() || "";
+    const n = (): number => Number(s());
+    const ss = (): string[] => s().split(" ");
+    const ns = (): number[] => ss().map(Number);
+    const nls = (): number[] => lines.map(Number);
+    const nla = (): number[][] =>
+      lines.map((line) => line.split(" ").map(Number));
+
+    return { lines, s, n, ss, ns, nls, nla };
+  };
+
+  const _res = main();
+
+  if (typeof _res === "number" || typeof _res === "string") console.log(_res);
+}
---
+++ src/abc207/c/main.ts
@@ -0,0 +1,62 @@
+"use strict";
+{
+  const main = (): number | string | void => {
+    const r = _io();
+    const n = r.n();
+    const NS = r.lines.map((line: string) => line.split(" ").map(Number));
+    const objs = [];
+    let count = 0;
+
+    for (let i = 0; i < n; i++) {
+      let start = 0,
+        end = 0;
+      switch (NS[i][0]) {
+        case 1:
+          start = NS[i][1];
+          end = NS[i][2];
+          break;
+        case 2:
+          start = NS[i][1];
+          end = NS[i][2] - 0.1;
+          break;
+        case 3:
+          start = NS[i][1] + 0.1;
+          end = NS[i][2];
+          break;
+        case 4:
+          start = NS[i][1] + 0.1;
+          end = NS[i][2] - 0.1;
+          break;
+      }
+      objs.push({ start, end });
+    }
+
+    for (let i = 0; i < n; i++) {
+      for (let j = i + 1; j < n; j++) {
+        if (objs[i].start <= objs[j].end && objs[i].end >= objs[j].start) {
+          count++;
+        }
+      }
+    }
+
+    return count;
+  };
+
+  const _io = () => {
+    const str: string = require("fs").readFileSync("/dev/stdin", "utf8");
+    const lines = str.trim().split("\n");
+    const s = (): string => lines.shift() || "";
+    const n = (): number => Number(s());
+    const ss = (): string[] => s().split(" ");
+    const ns = (): number[] => ss().map(Number);
+    const nls = (): number[] => lines.map(Number);
+    const nla = (): number[][] =>
+      lines.map((line) => line.split(" ").map(Number));
+
+    return { lines, s, n, ss, ns, nls, nla };
+  };
+
+  const _res = main();
+
+  if (typeof _res === "number" || typeof _res === "string") console.log(_res);
+}

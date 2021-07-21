339a5a53ea27d1702a5623996506ae819f852016
hir0o <mk.9.q.kyu@gmail.com>
2021-07-21 10:40:44 +0900

    update

---
+++ src/abc045/a/main.ts
@@ -0,0 +1,26 @@
+"use strict";
+{
+  const main = (): number | string | void => {
+    const r = _io();
+    const
+
+    return 0;
+  };
+
+  const _io = () => {
+    const str: string = require("fs").readFileSync("/dev/stdin", "utf8");
+    const lines = str.trim().split("\n");
+    const s = (): string => lines.shift() || "";
+    const n = (): number => Number(s());
+    const ss = (): string[] => s().split(" ");
+    const ns = (): number[] => ss().map(Number);
+    const nls = (): number[] => lines.map(Number);
+    const nla = (): number[][] => lines.map(line => line.split(" ").map(Number));
+
+    return { lines, s, n, ss, ns, nls, nla };
+  };
+
+  const _res = main();
+
+  if (typeof _res === "number" || typeof _res === "string") console.log(_res);
+}
